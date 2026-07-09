3a29b6fbdbaa7b5766857a5ad9d57fcec37e65ec
tgm-anaconda <tomg.m@icloud.com>
2026-07-09 23:24:43 +0200

    Delete netlify/functions directory

--- netlify/functions/vapid-key.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ publicKey: process.env.VAPID_PUBLIC_KEY }),
-  };
-};
