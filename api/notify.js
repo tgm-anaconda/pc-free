21cf22eb74d50db53620ceeefe5d56d03bea39bd
tgm-anaconda <tomg.m@icloud.com>
2026-07-10 00:55:04 +0200

    notify.js

--- api/notify.js
+++ api/notify.js
@@ -1,6 +1,14 @@
 const webpush = require('web-push');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
